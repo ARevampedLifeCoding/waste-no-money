89487e240fdf00e01d7214f1593d8a2aa2dde57e
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-09 10:32:42 -0500

    added to the db folder I am not sure if this is what is needed yet

--- public/db.js
+++ public/db.js
@@ -1,4 +1,10 @@
+const indexedDB = window.indexedDB || window.localStorage;
 
+let db;
 
-const indexedDB = window.indexedDB || window.localStorage
+const request = indexedDB.open("budget", 1);
 
+request.onupgradeneeded = (e) => {
+  const db = e.target.result;
+  db.creatObjectStore("pending", { autoIncrement: true });
+};
